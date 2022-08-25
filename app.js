e7c554020f3b12308ba77f34230f64daca523f58
Cameron <camsjobhunting@gmail.com>
2022-08-25 08:40:14 +0100

    base portfolio finished

---
+++ app.js
@@ -0,0 +1,5 @@
+// for now what i want to work won't work until i have it on an actual server
+
+//i'll need to have a server set up, have a folder with my images
+//include that folder with node
+//loop over it's conent then add the pictures as children to the .gridBody
