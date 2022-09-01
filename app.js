eb25a993c63628cdedda83849d719d65c0fc219e
Cameron <camsjobhunting@gmail.com>
2022-09-01 12:02:02 +0100

    add movies api, api appends to body

--- app.js
+++ app.js
@@ -3,3 +3,26 @@
 //i'll need to have a server set up, have a folder with my images
 //include that folder with node
 //loop over it's conent then add the pictures as children to the .gridBody
+
+const form = document.querySelector('#searchForm')
+
+const gridBody = document.querySelector('.gridbody')
+
+form.addEventListener('submit', async function (e) {
+    e.preventDefault();
+    const serchTerm = form.elements.query.value
+    const res = await axios.get(`https://api.tvmaze.com/search/shows?q=${serchTerm}`);
+    // console.log(res.data)
+    displayImages(res.data)
+    form.elements.query.value = "";
+})
+
+const displayImages = (shows) => {
+    for (let result of shows) {
+        if (result.show.image) {
+            const img = document.createElement('img');
+            img.src = result.show.image.medium;
+            document.body.append(img)
+        }
+    }
+}
